d5cf05a1d6eb98e63bb278d1df1325a6a075da69
AntPerc <antonio.percuoco@marconibari.edu.it>
2024-12-03 16:27:27 +0000

    finito

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,48 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+public class Program {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+    public static void main(String[] args) {
+        int i, j, n;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        System.out.println("Inserire il numero di elementi: ");
+        n = input.nextInt();
+        int[] u = new int[n], r = new int[n], s = new int[n];
+        double[] m = new double[n];
+
+        for (i = 0; i <= n - 1; i++) {
+            do {
+                System.out.println("Immetti il " + i + 1 + "° numero intero positivo: ");
+                u[i] = input.nextInt();
+            } while (u[i] < 0);
+        }
+        for (i = 0; i <= n - 1; i++) {
+            r[i] = random.nextInt(11);
+            System.out.println("Numero casuale in [0,10] avente " + i + 1 + "° posizione: " + r[i]);
+        }
+        for (i = 0; i <= n - 1; i++) {
+            s[i] = random.nextInt(8 - 3 + 1) + 3;
+            System.out.println("Numero casuale in [3,8] avente " + i + 1 + "° posizione: " + s[i]);
+        }
+        for (i = 0; i <= n - 1; i++) {
+            m[i] = (double) (u[i] + r[i] + s[i]) / 3;
+            System.out.println(Integer.toString(i + 1) + "° elemento media: " + m[i] + " = (" + u[i] + " + " + r[i] + " + " + s[i] + ")/3");
+        }
+        int t;
+
+        System.out.println("Inserire T: ");
+        t = input.nextInt();
+        System.out.println("Elementi di U con cui la loro somma sia: " + t);
+        for (i = 0; i <= n - 1; i++) {
+            for (j = 0; j <= n - 1; j++) {
+                if (u[i] + u[j] == t) {
+                    System.out.println(Integer.toString(u[i]) + " + " + u[j] + " = " + t);
+                }
+            }
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
